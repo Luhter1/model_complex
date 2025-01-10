1821c28d425f0525d923c752631f21c757d47334
Timofei <kaparulinti@mail.ru>
2025-01-10 15:10:39 +0300

    modificate forecast

--- model_complex/__init__.py
+++ model_complex/__init__.py
@@ -1,7 +1,7 @@
-from .calibration import Calibration, Forecats
+from .calibration import Calibration, Forecast
 from .models import FactoryBRModel
 from .epid_data import EpidData
 from .utils import ModelParams
 
 # Forecats!!!
-__all__ = ["Calibration", "Forecats", "EpidData", "FactoryBRModel", "ModelParams"]
+__all__ = ["Calibration", "Forecast", "EpidData", "FactoryBRModel", "ModelParams"]
--- model_complex/calibration/Forecast.py
+++ model_complex/calibration/Forecast.py
@@ -1,13 +1,11 @@
 import numpy as np
 
 from ..models import BRModel
+from ..utils import ModelParams
 
-class Forecats:
+class Forecast:
 
-    # alpha, beta
-    # data
-    # на сколько строить прогноз
-    # model
+    # TODO:     @classmethod
     def __init__(
         self,
         data: list,
@@ -36,13 +34,20 @@ class Forecats:
                 [[[float('inf'), float('-inf')] for _ in range(data_size)] for j in range(len(self.init_infectious))]
             )
 
+        simulate_pars = ModelParams(
+            alpha=[0],
+            beta=[0],
+            population_size=self.rho,
+            initial_infectious=self.init_infectious
+        )
+
         for a, b in zip(zip(*self.alpha), zip(*self.beta)):
 
+            simulate_pars.alpha = a
+            simulate_pars.beta = b
+            
             self.model.simulate(
-                alpha=a, 
-                beta=b, 
-                initial_infectious=self.init_infectious, 
-                rho=self.rho, 
+                pars=simulate_pars,
                 modeling_duration=data_size
             )
 
